79f723451eb32bf3a355b58e0881065f7f43201b
Anton <vip.valko@gmail.com>
2018-10-18 23:05:00 +0300

    add colors to map, add cell`s price

--- mapa.py
+++ mapa.py
@@ -92,8 +92,72 @@ def why():
     my.write('13', align='center', font=("Arial", 13, "normal"))
 
 
+    my.goto(-199, 102)
+    my.write('+100$ каждый круг', align='center', font=("Arial", 8, "normal"))
+
+    my.goto(-100, 102)
+    my.write('10$', align='center', font=("Arial", 10, "normal"))
+
+    my.goto(0, 102)
+    my.write('15$', align='center', font=("Arial", 10, "normal"))
+
+    my.goto(100, 102)
+    my.write('20$', align='center', font=("Arial", 10, "normal"))
+
+    my.goto(200, 102)
+    my.write('25$', align='center', font=("Arial", 10, "normal"))
+
+    my.goto(200, 2)
+    my.write('30$', align='center', font=("Arial", 10, "normal"))
+
+    my.goto(200, -98)
+    my.write('35$', align='center', font=("Arial", 10, "normal"))
+
+    my.goto(200, -198)
+    my.write('40$', align='center', font=("Arial", 10, "normal"))
+
+    my.goto(100, -198)
+    my.write('45$', align='center', font=("Arial", 10, "normal"))
+
+    my.goto(0, -198)
+    my.write('50$', align='center', font=("Arial", 10, "normal"))
+
+    my.goto(-100, -198)
+    my.write('55$', align='center', font=("Arial", 10, "normal"))
+
+    my.goto(-200, -198)
+    my.write('60$', align='center', font=("Arial", 10, "normal"))
+
+    my.goto(-200, -98)
+    my.write('65$', align='center', font=("Arial", 10, "normal"))
+
+    my.goto(-200, 2)
+    my.write('70$', align='center', font=("Arial", 10, "normal"))
+
+    my.goto(0, 212)
+    my.color('Red')
+    my.begin_fill()
+
+    my.setheading(0)
+    my.forward(116)
+    my.left(90)
+    my.forward(38)
+    my.left(90)
+    my.forward(232)
+    my.left(90)
+    my.forward(38)
+    my.left(90)
+    my.forward(116)
+
+    my.end_fill()
+    my.color('white')
+    my.write('МОНОПОЛИЯ', align="center", font=("Arial", 25, "bold"))
+
+
 if __name__ == '__main__':
     import turtle
     window = turtle.Screen()
+    window.colormode(255)
+    window.bgcolor(245, 255, 245)
     why()
     window.exitonclick()
